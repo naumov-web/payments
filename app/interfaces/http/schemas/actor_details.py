88f7d6ff11c94c9639b41e54a93dde9522085939
Konstantin Naumov <k.naumov@sgmsoft.com>
2026-05-27 16:15:24 +0300

    P-28: Get actor by id

---
+++ app/interfaces/http/schemas/actor_details.py
@@ -0,0 +1,17 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class ActorDetailsResponse(
+    BaseModel
+):
+    actor_id: UUID
+
+    name: str
+
+    email: str
+
+    role: str
+
+    balance: int
